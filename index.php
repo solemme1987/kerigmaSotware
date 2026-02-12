97f5b98a5c70d2b0e816d988ed9e06d5fa4d4a93
Ricardo Cores Hurtado <ricacor3@gmail.com>
2026-02-12 15:50:03 -0500

    Split content plus finished

--- index.php
+++ index.php
@@ -18,59 +18,12 @@
   get_template_part('src/pages/components/homeHero');
 
   // Services component
-  get_template_part('src/pages/components/services')
+  get_template_part('src/pages/components/services');
+
+  // Split content plus component
+  get_template_part('src/pages/components/splitContentPlus');
   ?>
 
-  <!--
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 5 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 6 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 7 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 8 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 9 Aqui estamos probando que todo marchi bien item 1</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 10</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 11</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 12</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 13</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 14</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 15</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 16</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 17</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 18</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 1 Aqui estamos probando que todo marchi bien item 19</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 10</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 11</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 12</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 13</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 14</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 15</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 16</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 17</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 18</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 2 Aqui estamos probando que todo marchi bien item 19</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 10</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 11</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 12</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 13</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 14</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 15</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 16</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 17</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 18</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 3 Aqui estamos probando que todo marchi bien item 19</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 10</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 11</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 12</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 13</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 14</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 15</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 16</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 17</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 18</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 4 Aqui estamos probando que todo marchi bien item 19</h2>
-  <h2>Aqui estamos probando que todo marchi bien item 5 Aqui estamos probando que todo marchi bien item 10</h2>-->
+
 </main>
 <?php get_footer(); ?>
---
+++ src/pages/components/splitContentPlus.php
@@ -0,0 +1,51 @@
+<section class="o-container o-container--split-content-plus u-bg-primary">
+  <div class="c-split-content-plus o-row ">
+
+    <div class="c-split-content-plus__content">
+
+      <span class="c-split-content-plus__title"> ¿cómo lo hacemos?</span>
+
+      <h2 class="c-split-content-plus__heading">
+        Colaboración estrecha en cada etapa
+      </h2>
+
+      <p class="c-split-content-plus__text">
+        Disponemos de un equipo de especialistas dedicados a cada fase del proyecto. Trabajamos en estrecha colaboración contigo, brindando un sólido apoyo desde la consultoría inicial hasta el soporte post-entrega.
+      </p>
+
+      <div class="c-split-content-plus__card-group">
+        <article class="c-split-content-plus__card">
+          <span class="material-icons-round u-color-secondary c-split-content-plus__icon">psychology</span>
+          <h2 class="c-split-content-plus__card-title">consultoría</h2>
+          <p class="c-split-content-plus__card-text">
+            Analizamos tu entorno y diseñamos soluciones alineadas a tus objetivos
+          </p>
+        </article>
+
+        <article class="c-split-content-plus__card">
+          <span class="material-icons-round u-color-secondary c-split-content-plus__icon">code</span>
+          <h2 class="c-split-content-plus__card-title">deasarrollo</h2>
+          <p class="c-split-content-plus__card-text">
+            Sistemas a medida, escalables y con las mejores prácticas de arquitectura.
+          </p>
+        </article>
+
+      </div>
+
+    </div>
+
+    <div class="c-split-content-plus__img-container">
+      <img src="https://picsum.photos/480/480" alt="image" srcset="">
+
+      <article class="c-split-content-plus__card c-split-content-plus__card--img ">
+        <span class="c-split-content-plus__card-counter u-color-primary">10+</span>
+        <h2 class="c-split-content-plus__card-title">Años de Experiencia
+
+        </h2>
+        <p class="c-split-content-plus__card-text">
+          En desarrollo de software de alto impacto.
+        </p>
+      </article>
+    </div>
+  </div>
+</section>
